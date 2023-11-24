689786768d61a046b8ac41d964f4c85b498a6804
yan05021 <justyiyan@gmail.com>
2023-11-24 22:26:21 +0100

    page contact avec formulaire fonctionnelle

--- back/index.js
+++ back/index.js
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from "express";
 
-import getDraft from './routes/getDraft.js';
+import getDraft from "./routes/getDraft.js";
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const app = express();
 
-app.get('/form', getDraft);
+app.get("/form", getDraft);
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ front/src/components/ContactPage.jsx
@@ -0,0 +1,188 @@
+import { useForm } from "react-hook-form";
+
+function ContactPage() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <>
+            <div className="ContactPage">
+                <div className="ContactPage_info">
+                    <div className="ContactPage_info_caution">
+                        <p>
+                            La mise à jour des données est un dialogue entre
+                            votre entreprise et OpenClimat qui vérifie les
+                            données. Aucune information soumise dans ce
+                            formulaire ne sera publiée sans discussion
+                            préalable.
+                        </p>
+                        <p>
+                            Prochaine analyse: <span>Décembre 2023</span>
+                        </p>
+                    </div>
+                    <div className="ContactPage_info_update">
+                        <p>
+                            Vous souhaitez demander une mise à jour de votre
+                            page publique OpenClimat?
+                        </p>
+                        <p>
+                            Voici les éléments que vous pouvez mettre à jour
+                            (tout ou parite)
+                        </p>
+                        <ul>
+                            <li>
+                                Performance climatique (% de réduction des
+                                émissions sur vos scopes 1&2 et 3)
+                            </li>
+                            <li>
+                                Bilan carbone (données brutes de vos émissions
+                                de CO2 sur vos scopes 1&3 et 3)
+                            </li>
+                            <li>Labels liés au climat (SBTi, CDP)</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <form
+                    className="ContactPage_form"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <p>Contact</p>
+
+                    {/* ContactPage_form_lastname */}
+                    <div className="ContactPage_form_lastname">
+                        <label htmlFor="lastname">Nom</label>
+                        <input
+                            type="text"
+                            className="ContactPage_form_input"
+                            {...register("lastname", {
+                                required: "Ce champ est requis",
+                                pattern: {
+                                    value: /^[A-Za-zÀ-ÿ ]+$/i,
+                                    message:
+                                        "Caractères alphabétiques uniquement",
+                                },
+                            })}
+                            placeholder="Nom ..."
+                        />
+                    </div>
+                    {/* Affichage des erreurs pour le champ lastname */}
+                    {errors.lastname && (
+                        <span className="ContactForm_error">
+                            {errors.lastname.message}
+                        </span>
+                    )}
+
+                    {/* ContactPage_form_firstname */}
+                    <div className="ContactPage_form_prenom">
+                        <label htmlFor="firstname">Prénom</label>
+                        <input
+                            type="text"
+                            className="ContactPage_form_input"
+                            {...register("firstname", {
+                                required: "Ce champ est requis",
+                                pattern: {
+                                    value: /^[A-Za-zÀ-ÿ ]+$/i,
+                                    message:
+                                        "Caractères alphabétiques uniquement",
+                                },
+                            })}
+                            placeholder="Prénom ..."
+                        />
+                    </div>
+                    {/* Affichage des erreurs pour le champ firstname */}
+                    {errors.firstname && (
+                        <span className="ContactForm_error">
+                            {errors.firstname.message}
+                        </span>
+                    )}
+
+                    {/* ContactPage_form_jobtitle */}
+                    <div className="ContactPage_form_prenom">
+                        <label htmlFor="jobtitle">Votre poste</label>
+                        <input
+                            type="text"
+                            className="ContactPage_form_input"
+                            {...register("jobtitle", {
+                                required: "Ce champ est requis",
+                                pattern: {
+                                    value: /^[A-Za-zÀ-ÿ ]+$/i,
+                                    message:
+                                        "Caractères alphabétiques uniquement",
+                                },
+                            })}
+                            placeholder="Intitulé poste ..."
+                        />
+                    </div>
+                    {/* Affichage des erreurs pour le champ jobtitle */}
+                    {errors.jobtitle && (
+                        <span className="ContactForm_error">
+                            {errors.jobtitle.message}
+                        </span>
+                    )}
+
+                    {/* ContactPage_form_email */}
+                    <div className="ContactPage_form_email">
+                        <label htmlFor="email">Adresse email</label>
+                        <input
+                            type="text"
+                            className="ContactPage_form_input"
+                            {...register("email", {
+                                required: "Ce champ est requis",
+                                pattern: {
+                                    value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
+                                    message:
+                                        "Merci de renseigner un email valide",
+                                },
+                            })}
+                            placeholder="Email"
+                        />
+                    </div>
+                    {/* Affichage des erreurs pour le champ email */}
+                    {errors.email && (
+                        <span className="ContactForm_error">
+                            {errors.email.message}
+                        </span>
+                    )}
+
+                    {/* ContactPage_form_phonenumber */}
+                    <div className="ContactPage_form_phonenumber">
+                        <label htmlFor="phonenumber">Numéro de téléphone</label>
+                        <input
+                            type="text"
+                            className="ContactPage_form_input"
+                            {...register("phonenumber", {
+                                required: "Ce champ est requis",
+                                pattern: {
+                                    value: /^(0|\+33)?[1-9][0-9]{8}$/,
+                                    message:
+                                        "Merci de renseigner un numéro de téléphone valide",
+                                },
+                            })}
+                            placeholder="Numéro ..."
+                        />
+                    </div>
+                    {/* Affichage des erreurs pour le champ phonenumber */}
+                    {errors.phonenumber && (
+                        <span className="ContactForm_error">
+                            {errors.phonenumber.message}
+                        </span>
+                    )}
+
+                    <button className="ContactPage_form_button" type="submit">
+                        Commencer
+                    </button>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default ContactPage;
